008f58fce78734373ce24a7502c7f5adc028a708
DeekshithaDamma <135822283+DeekshithaDamma@users.noreply.github.com>
2023-07-19 19:01:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function cyndrila(){
+    document.getElementById("damma").src="https://i.pinimg.com/originals/62/b2/14/62b214cc455f8f719237984a53431ff8.png";
+}
+function jery(){
+    document.getElementById("damma").src="https://drawinghowtos.com/wp-content/uploads/2021/12/Tom-colored.png";
+}
+function dora(){
+    document.getElementById("damma").src="https://upload.wikimedia.org/wikipedia/en/thumb/b/bd/Doraemon_character.png/220px-Doraemon_character.png";
+}
